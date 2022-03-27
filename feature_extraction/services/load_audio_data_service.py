87dd3a4d42bbce3b0ab0bdbd17c3d41ea91f5dc7
joe <joe.zhangyuechen@hotmail.com>
2022-03-27 16:34:25 -0400

    start on feature extraction

---
+++ feature_extraction/services/load_audio_data_service.py
@@ -0,0 +1,25 @@
+from ast import Load
+import librosa
+
+class LoadAudioDataService:
+    def __init__(self, path):
+        self.path = path
+
+    def load(self, **args):
+        self.y, self.sr = librosa.load(self.path, **args)
+        return self
+    
+    def trim(self, **args):
+        self.y, index = librosa.effects.trim(self.y, **args)
+        return self
+
+    
+
+
+
+if __name__ == "__main__":
+    audio = LoadAudioDataService('feature_extraction/data/PC1_20090513_050000_0010.wav')
+    audio.load().trim()
+    
+
+
