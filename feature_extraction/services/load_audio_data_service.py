efc092569413d22662d7be4418868de08e8fd74c
joe <joe.zhangyuechen@hotmail.com>
2022-03-27 23:27:54 -0400

    sound extraction using fft initialization

--- feature_extraction/services/load_audio_data_service.py
+++ feature_extraction/services/load_audio_data_service.py
@@ -1,5 +1,7 @@
-from ast import Load
+import matplotlib.pyplot as plt
 import librosa
+import librosa.display
+import IPython.display as ipd
 
 class LoadAudioDataService:
     def __init__(self, path):
@@ -12,14 +14,19 @@ class LoadAudioDataService:
     def trim(self, **args):
         self.y, index = librosa.effects.trim(self.y, **args)
         return self
-
     
-
+    def display(self, **args):
+        plt.figure(figsize=(14, 5))
+        librosa.display.waveshow(self.y, sr=self.sr, **args)
+    
+    def play(self):
+        ipd.Audio(self.y, rate=self.sr)
 
 
 if __name__ == "__main__":
     audio = LoadAudioDataService('feature_extraction/data/PC1_20090513_050000_0010.wav')
-    audio.load().trim()
+    audio.load().trim().display()
+
     
 
 
