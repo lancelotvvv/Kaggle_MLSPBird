0184d6e88ce1c0283bdc1b2a841cdb5dfba3a056
joe <joe.zhangyuechen@hotmail.com>
2022-04-04 22:54:18 -0400

    added hpss filter

--- feature_extraction/services/load_audio_data_service.py
+++ feature_extraction/services/load_audio_data_service.py
@@ -1,13 +1,16 @@
+from ast import Load
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 import IPython.display as ipd
+import numpy as np
 
 class LoadAudioDataService:
-    def __init__(self, path):
-        self.path = path
+    dB = None
+    stft = None
 
-    def load(self, **args):
+    def load(self, path, **args):
+        self.path = path
         self.y, self.sr = librosa.load(self.path, **args)
         return self
     
@@ -15,12 +18,47 @@ class LoadAudioDataService:
         self.y, index = librosa.effects.trim(self.y, **args)
         return self
     
-    def display(self, **args):
-        plt.figure(figsize=(14, 5))
-        librosa.display.waveshow(self.y, sr=self.sr, **args)
-    
+    def display(self, type = 'wave',**args):
+        if type == 'wave':
+            librosa.display.waveshow(self.y, sr=self.sr, **args)
+        elif type == 'spec':
+            if self.dB is None:
+                self.to_db()
+            librosa.display.specshow(self.dB, sr=self.sr, **args)
+        else:
+            librosa.display.waveshow(self.y, sr=self.sr, **args)
+
     def play(self):
-        ipd.Audio(self.y, rate=self.sr)
+        return ipd.Audio(self.y, rate=self.sr)
+
+    def to_stft(self):
+        self.stft = librosa.stft(self.y)
+    
+    def to_db(self):
+        if self.stft is None:
+            self.to_stft()
+        self.dB = librosa.amplitude_to_db(np.abs(self.stft))
+    
+    def hpss_filter(self, **args):
+        if self.stft is None:
+            self.to_stft()
+        H, P = librosa.decompose.hpss(self.stft)
+        
+        h = librosa.istft(H)
+        p = librosa.istft(P)
+
+        HAudio = LoadAudioDataService()
+        HAudio.sr = self.sr
+        HAudio.stft = H
+        HAudio.y = h
+
+        PAudio = LoadAudioDataService()
+        PAudio.sr = self.sr
+        PAudio.stft = P
+        PAudio.y = p
+
+        return HAudio, PAudio
+
 
 
 if __name__ == "__main__":
